ef94bd6a57ba99ba7674873dc9fc04ee6642c793
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-09-26 21:48:32 +0000

    migrate routing: add BrowserRouter provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { trpc } from "@/lib/trpc";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import superjson from "superjson";
 import App from "./App";
 import "./index.css";
@@ -38,7 +39,9 @@ const trpcClient = trpc.createClient({
 createRoot(document.getElementById("root")!).render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </trpc.Provider>
 );
